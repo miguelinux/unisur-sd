1878c4e4de050d03473db19799c4892654edee9a
Miriam Díaz Rodríguez <miriam.diaz@zapopan.tecmm.edu.mx>
2023-05-11 20:11:57 -0600

    t12

--- Restful.php
+++ Restful.php
@@ -22,12 +22,12 @@ $mdb = json_decode(file_get_contents("https://www.omdbapi.com/?s=galaxy&apikey=2
 $data=$mdb['Search'];
 //var_dump($data)
 
-echo $data[0]["Title"]
-/*
-for ($i = 1; $i <= 10; $i++) {
-  echo $i;
+for ($i = 0; $i <= 3; $i++) {
+  echo $data[$i]["Title"]
+  echo "<br>"
 }
 
+/*
 for ($i = 0; $i < 5; $i = $i + 1 )  {
   echo $data[$i]["Title"]
   echo "Titulo: ".$data[$i]["Title"]."<br>"
